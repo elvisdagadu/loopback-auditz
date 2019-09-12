3d22cd51963ec0358e378715a9b14fd9b5e9e082
Edzordzinam E Dagadu <developer@edzordzinam.me>
2019-09-12 04:16:08 +0000

    Model properties added ObjectId type

--- src/auditz.js
+++ src/auditz.js
@@ -10,34 +10,34 @@ const warn = (options, ...rest) => {
 
 Object.compare = function(obj1, obj2) {
 	//Loop through properties in object 1
-	              for (var p in obj1) {
+	                for (var p in obj1) {
 		//Check property exists on both objects
-		              if (obj1.hasOwnProperty(p) !== obj2.hasOwnProperty(p)) return false;
+		                if (obj1.hasOwnProperty(p) !== obj2.hasOwnProperty(p)) return false;
   if (obj1[p] === null || obj2[p] === null) {
     return obj1[p] === obj2[p];
   }
 
-		              switch (typeof (obj1[p])) {
+		                switch (typeof (obj1[p])) {
 			//Deep compare objects
 			case 'object':
   if (typeof (obj2[p]) !== 'object') return false;
-				              if (!Object.compare(obj1[p], obj2[p])) return false;
-				              break;
+				                if (!Object.compare(obj1[p], obj2[p])) return false;
+				                break;
 			//Compare function code
 			case 'function':
-				              if (typeof (obj2[p]) === 'undefined' || (p !== 'compare' && obj1[p].toString() !== obj2[p].toString())) return false;
-				              break;
+				                if (typeof (obj2[p]) === 'undefined' || (p !== 'compare' && obj1[p].toString() !== obj2[p].toString())) return false;
+				                break;
 			//Compare values
 			default:
-				              if (obj1[p] !== obj2[p]) return false;
+				                if (obj1[p] !== obj2[p]) return false;
 		}
 	}
 
 	//Check object 2 for any extra properties
-	              for (var p in obj2) {
-		              if (typeof (obj1[p]) === 'undefined') return false;
+	                for (var p in obj2) {
+		                if (typeof (obj1[p]) === 'undefined') return false;
 	}
-	              return true;
+	                return true;
 };
 
 export default (Model, bootOptions = {}) => {
@@ -111,13 +111,13 @@ export default (Model, bootOptions = {}) => {
 
   if (options.createdBy !== false) {
     if (typeof(properties[options.createdBy]) === 'undefined') {
-      Model.defineProperty(options.createdBy, {type: String, required: false});
+      Model.defineProperty(options.createdBy, {type: String, required: false, mongodb: {dataType: 'ObjectID'}});
     }
   }
 
   if (options.updatedBy !== false) {
     if (typeof(properties[options.updatedBy]) === 'undefined') {
-      Model.defineProperty(options.updatedBy, {type: String, required: false});
+      Model.defineProperty(options.updatedBy, {type: String, required: false, mongodb: {dataType: 'ObjectID'}});
     }
   }
 
@@ -126,7 +126,7 @@ export default (Model, bootOptions = {}) => {
       Model.defineProperty(options.deletedAt, { type: Date, required: false, 'default': null });
     }
     if (typeof(properties[options.deletedBy]) === 'undefined') {
-      Model.defineProperty(options.deletedBy, {type: String, required: false});
+      Model.defineProperty(options.deletedBy, {type: String, required: false, mongodb: {dataType: 'ObjectID'}});
     }
   }
 
@@ -193,10 +193,10 @@ export default (Model, bootOptions = {}) => {
         //this is to allow adding data from remoting context to the revisions model
         if (options.revisions.remoteContextData && options.revisions.remoteContextData.length > 0) {
           options.revisions.remoteContextData.forEach((property) => {
-             if (ctx.options[property]){
+            if (ctx.options[property]) {
                data[property] = ctx.options[property];
              }
-           });
+          });
         }
 
         app.models[options.revisionsModelName].create(data, saveGroups);
